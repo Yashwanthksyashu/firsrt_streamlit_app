cffc124aaf2da7a4b4ea8b67a6b2e715c64769c0
yashwanth <111725898+Yashwanthksyashu@users.noreply.github.com>
2022-09-15 13:35:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,3 +60,89 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
     return fruityvice_normalized
+
+
+#New section to display fruitvise api response
+
+streamlit.header('Fruityvise Fruit Advise!')
+
+try:
+
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+
+   if not fruit_choice:
+
+         streamlit.error("Please  select a fruit to get information.")
+
+   else:
+
+       back_from_function = get_fruityvice_data(fruit_choice)
+
+       streamlit.dataframe(back_from_function)
+
+      
+
+except URLError as e:
+
+  streamlit.error()
+
+
+
+streamlit.header('View Our fruit List - Add Your Favourites!')
+
+#Snowflake-related functions
+
+def get_fruit_load_list():
+
+    with my_cnx.cursor() as my_cur:
+
+         my_cur.execute('select * from fruit_load_list')
+
+         return my_cur.fetchall()  
+
+
+
+#Add a button to load the fruit
+
+if streamlit.button('Get Fruit List'):
+
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+   my_data_rows = get_fruit_load_list()
+
+   my_cnx.close()
+
+   streamlit.dataframe(my_data_rows)
+
+    
+
+    # don't run anything past here while we troubleshoot
+
+streamlit.stop()
+
+
+
+# Allow the end user to add a fruit to the list
+
+def insert_row_snowflake(new_fruit):
+
+    with my_cnx.cursor() as my_cur:
+
+         my_cur.execute("insert into FRUIT_LOAD_LIST values (''jackfruit', 'papaya', 'guava' and 'kiwi'')")
+
+         return "Thanks for adding " + new_fruit
+
+        
+
+add_my_fruit = streamlit.text_input('What fruit would you like like to add?')
+
+if streamlit.button('Add a Fruit to the List'):
+
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+   back_from_function = insert_row_snowflake(add_my_fruit)
+
+   my_cnx.close()
+
+   streamlit.text(back_from_function)
+
