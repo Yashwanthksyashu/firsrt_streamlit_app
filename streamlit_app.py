6c5fbf76f99219a08825d0ced3d527b66ddd686c
yashwanth <111725898+Yashwanthksyashu@users.noreply.github.com>
2022-09-15 13:12:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
-#requirements.txt
+requirements.txt
 import snowflake.connector
 
 
