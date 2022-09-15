0fda1b3964e30525200e9cc7a3225d2ffb4fd14d
yashwanth <111725898+Yashwanthksyashu@users.noreply.github.com>
2022-09-15 13:34:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,62 @@
 import streamlit
 
 
-streamlit.title('my parents new healthy dinner')
+import pandas
+
+import requests
+
+import snowflake.connector
+
+from urllib.error import URLError
+
+
+
+streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥬 Kale,Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard - Boiled Free - Range Egg')
-streamlit.text('🍐🍞 Avokado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 
-import pandas 
+streamlit.text('🥗 kale,Spinach and Rocket Smoothie')
+
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.text('🥑 Avocado Toast')
+
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+
+#import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
-requirements.txt
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
-import snowflake.connector
 
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_data_row = my_cur.fetchone()
-#streamlit.text("Hello from Snowflake:")
-#streamlit.text(my_data_row)
+# Display the table on the page.
+
+streamlit.dataframe(fruits_to_show)
+
+
+
+#Create a repeatable code block (called a function)
+
+def get_fruityvice_data(this_fruit_choice):
+
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+    return fruityvice_normalized
